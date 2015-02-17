d98181f5a76d8714ec4a881d9c173ac2cf9c09a9
Shubham Pathak <shubham-pathak@users.noreply.github.com>
2015-02-17 18:25:42 +0530

    Create Constants.java

---
+++ src/main/java/com/regression/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.regression.utils;
+
+public class Constants {
+	
+	
+	public final static String WSDL_ARISHOP = "http://172.16.10.37:8080/ARIShopService-WS-1.0-SNAPSHOT/services/ARIShopService";
+
+}
