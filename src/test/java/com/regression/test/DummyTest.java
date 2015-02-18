bc73f59e1f01af1f8dbd0145a89553018c5cccac
Shubham Pathak <shubham-pathak@users.noreply.github.com>
2015-02-18 12:58:41 +0530

    Create DummyTest.java

---
+++ src/test/java/com/regression/test/DummyTest.java
@@ -0,0 +1,36 @@
+package com.regression.test;
+
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+import com.regression.ARIShopRequest;
+import com.regression.xmlComparator.XMLComparator;
+
+public class DummyTest {
+	
+	ARIShopRequest ar = new ARIShopRequest();
+	XMLComparator  xc = new XMLComparator();
+
+	@Test
+	public void dummy_test_1(){
+
+
+		try {
+			System.out.println("Send AriShop Request");
+			ar.getAriShopResponse();
+			boolean actual=xc.compareXml(ar.actualResponsePath, ar.expectedResponsePath);
+			Assert.assertEquals(actual, true);
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+}
